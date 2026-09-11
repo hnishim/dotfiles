7d6a34a40d040caec4ff14be5eec3bcec24fe528
Hiro Nishimiya <02-hint-oaken@icloud.com>
2026-09-11 18:14:23 +0900

    hs.ipc

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,4 @@
+require("hs.ipc")
 local targetDirectory = os.getenv("HOME") .. "/Library/Mobile Documents/com~apple~CloudDocs/Dev/scripts/hammerspoon"
 package.path = targetDirectory .. "/?.lua;" .. package.path
 dofile(targetDirectory .. "/main.lua")
